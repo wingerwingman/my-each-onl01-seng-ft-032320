acd00b729c5e820f37bc8a2cf0d100ccf1854ebe
Justin Winger <justinwinger4@gmail.com>
2020-03-25 18:32:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,6 @@
 def my_each(collection)
   if block_given?
+    i = 0
     counter = 0
   while i < collection.length
   yield(array[i])
@@ -11,6 +12,8 @@ else
 end
   
 end
+
+my_each(list) {|i| puts "here is #{i}."
 
 # my_each(words) do |word|
 
